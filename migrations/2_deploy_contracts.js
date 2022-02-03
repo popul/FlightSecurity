d590f032e02dd1031c1f5c2355ebe8db8d84f005
Paul Musso <paul@musso.io>
2022-02-03 21:26:51 +0100

    chore: deploy and configure smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,32 @@
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
-const fs = require('fs');
+const fs = require("fs");
 
-module.exports = function(deployer) {
+module.exports = async function (deployer) {
+  let firstAirline = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+  await deployer.deploy(FlightSuretyData, firstAirline, "Air France");
+  await deployer.deploy(FlightSuretyApp);
+  const dataContract = await FlightSuretyData.deployed()
+  await dataContract.authorizeCaller(FlightSuretyApp.address);
+  const appContract = await FlightSuretyApp.deployed()
+  await appContract.setDataContract(FlightSuretyData.address);
 
-    let firstAirline = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
-    deployer.deploy(FlightSuretyData, firstAirline, "Air France")
-    .then(() => {
-        return deployer.deploy(FlightSuretyApp)
-                .then(() => {
-                    let config = {
-                        localhost: {
-                            url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
-                        }
-                    }
-                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                });
-    });
-}
+  let config = {
+    localhost: {
+      url: "http://localhost:8545",
+      dataAddress: FlightSuretyData.address,
+      appAddress: FlightSuretyApp.address,
+    },
+  };
+
+  fs.writeFileSync(
+    __dirname + "/../src/dapp/config.json",
+    JSON.stringify(config, null, "\t"),
+    "utf-8"
+  );
+  fs.writeFileSync(
+    __dirname + "/../src/server/config.json",
+    JSON.stringify(config, null, "\t"),
+    "utf-8"
+  );
+};
